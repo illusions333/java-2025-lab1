b079e110220c044fe66b6c1b28b1244304b4b7af
illusions333 <1alexillusions1@gmail.com>
2025-09-29 19:14:43 +0200

    Fix the issue with the Main.java

--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,44 +1,103 @@
 package ua.university;
 
 import ua.university.model.*;
-import ua.university.service.ReportGenerator;
+import ua.university.service.RoomListGenerator;
+import ua.university.service.TotalAmountGenerator;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+        Person person1 = new Person("Oleksandr", "Dubchak", "dubchak.oleksandr@chnu.edu.ua");
+        Person person2 = new Person("Artem", "Shkrebko", "notValidMail"); //email is not valid, so it would be replaced by null
+        System.out.println(person1);
+        System.out.println(person2);
 
-        Teacher teacher1 = new Teacher("Іван", "Петренко", "math@univ.edu", "Математичний", "Доцент");
-        Teacher teacher2 = new Teacher("Марія", "Сидоренко", "phys@univ.edu", "Фізичний", "Професор");
+//        System.out.println(person1.getFullName()); getFullName is protected, so we cannot access it through Main
+//        System.out.println(person1.firstName); firstName is also protected, so it can't be accessed either
 
-        Group cs101 = new Group(101, "Computer Science", 2023, 30);
+        System.out.println(person2.getEmail());
 
-        Student student1 = new Student("Олексій", "Іванов", "alex@univ.edu", "ST001", cs101);
-        Student student2 = new Student("Анна", "Коваль", "anna@univ.edu", "ST002", cs101);
-        Student student3 = new Student("Ігор", "Лисенко", "igor@univ.edu", "ST003", cs101);
+        Person person3 = new Person();
+        System.out.println(person3);
 
-        Subject math = new Subject("Математика", 5);
-        Subject physics = new Subject("Фізика", 4);
+        Person person4 = new Person("Oleksandr", "Dubchak", "dubchak.oleksandr@chnu.edu.ua");
+        System.out.println(person4.equals(person1));
+        System.out.println(person4.equals(person1)); //checking for consistency
+        System.out.println(person1.equals(person4)); //checking for commutativity
+        System.out.println(person4.equals(person2));
 
-        StudentGrade[] grades = {
-                new StudentGrade(student1, math, teacher1, Grade.A, 95, ExamType.EXAM, "Fall", LocalDate.of(2025, 6, 10)),
-                new StudentGrade(student1, physics, teacher2, Grade.B, 85, ExamType.CREDIT, "Fall", LocalDate.of(2025, 6, 12)),
-                new StudentGrade(student2, math, teacher1, Grade.C, 75, ExamType.EXAM, "Fall", LocalDate.of(2025, 6, 10)),
-                new StudentGrade(student2, physics, teacher2, Grade.F, 40, ExamType.DIFFERENTIATED_CREDIT, "Fall", LocalDate.of(2025, 6, 12)),
-                new StudentGrade(student3, math, teacher1, Grade.B, 88, ExamType.LABORATORY, "Fall", LocalDate.of(2025, 6, 11)),
-                new StudentGrade(student3, physics, teacher2, Grade.D, 65, ExamType.COURSEWORK, "Fall", LocalDate.of(2025, 6, 13))
-        };
+        Person person5 = Person.createPerson("Oleksandr", "Dubchak");
+        System.out.println(person5);
 
-        System.out.println("=== Індивідуальні звіти студентів ===");
-        for (StudentGrade grade : grades) {
-            System.out.println(ReportGenerator.formatExamResult(grade));
-        }
 
-        System.out.println("\n=== Повний звіт студентів ===");
-        System.out.println(ReportGenerator.generateStudentReport(grades));
+        System.out.println();
 
-        System.out.println("\n=== Аналіз групи ===");
-        System.out.println(ReportGenerator.analyzeGroupPerformance(grades));
+
+        Room room1 = new Room();
+        System.out.println(room1);
+        Room room2 = Room.createRoom(121, "Presidential", 5, 1000, RoomStatus.CLEANING);
+        System.out.println(room2);
+        Room room3 = new Room(15, "", 200, -1, RoomStatus.MAINTENANCE);
+        /*capacity can't be more than 6, so it would show 0 instead
+        price can't also be negative, so 0 would be shown*/
+        System.out.println(room3);
+//        System.out.println(room1.roomNumber) room number is a private field, which can't be accessed outside the class
+
+
+        System.out.println();
+
+
+        Service service1 = new Service("Cleaning", 500);
+        System.out.println(service1);
+        Service service2 = new Service("Fixing the toilet", 1000);
+        System.out.println(service2);
+        Service service3 = new Service("Fixing the fridge", 1500);
+        System.out.println(service3);
+        Service service4 = new Service("Providing an AC", 1000);
+        System.out.println(service4);
+
+
+        System.out.println();
+
+
+        Guest guest1 = new Guest(person1, LocalDate.of(2025, 8, 11));
+        System.out.println(guest1);
+        System.out.println();
+        Guest guest2 = new Guest(person2, LocalDate.of(2025, 8, 11));
+        System.out.println(guest2);
+
+
+        System.out.println();
+
+
+        Reservation reservation1 = new Reservation(guest1, room2, LocalDate.of(2025, 8, 11), LocalDate.of(2025, 8, 21), ReservationStatus.CONFIRMED);
+        System.out.println(reservation1);
+
+        Reservation reservation2 = Reservation.createReservation(guest1, room2, LocalDate.of(2025, 8, 11), LocalDate.of(2025, 8, 21), ReservationStatus.CONFIRMED);
+        System.out.println(reservation2);
+
+        Reservation reservation3 = new Reservation();
+        System.out.println(reservation3);
+
+        System.out.println(reservation1.equals(reservation2));
+        System.out.println(reservation1.getDuration());
+
+
+        System.out.println();
+
+
+        Invoice invoice1 = new Invoice(reservation1, LocalDate.of(2025, 8, 21), 2500);
+        System.out.println(invoice1);
+        Invoice invoice2 = Invoice.createInvoice(reservation2, LocalDate.of(2025, 8, 21), 2500);
+        System.out.println(invoice1.equals(invoice2));
+        System.out.println(invoice1.getDailySpending());
+/*        boolean notValidBool = ValidationHelper.isValidDate(guest1.getCheckInDate()); ValidationHelper is a package-private class and can't be accessed from Main
+          other utils are public, that's why we can access them if we import those classes
+ */
+        System.out.println();
+        System.out.println(RoomListGenerator.generateRoomList(new Room[]{room1, room2, room3}));
+        System.out.println();
+        System.out.println(TotalAmountGenerator.generateTotalAmount(reservation1, new Service[]{service1, service2, service3, service4}));
     }
 }
-
